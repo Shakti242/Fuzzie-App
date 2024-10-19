986568f7af70b1f8311a126a4149909a13d088fb
Shakti242 <shaktipatel2421@gmail.com>
2024-10-19 15:29:45 -0400

    csz

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,14 +5,16 @@ import React from 'react'
 type Props = { children: React.ReactNode }
 
 const Layout = (props: Props) => {
-  return (
-    <div className="flex overflow-hidden h-screen">
-      <Sidebar />
-      <div className="w-full"> 
-        {props.children}
-      </div>
-    </div>
-  )
+    const a = 3
+    console.log(a)
+    return (
+        <div className="flex overflow-hidden h-screen">
+            <Sidebar />
+            <div className="w-full">
+                {props.children}
+            </div>
+        </div>
+    )
 }
 
 export default Layout
